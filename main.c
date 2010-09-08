6a48fac3d48357ce880d72e926049961d71d3d51
halos <halos@lambda>
2010-09-08 16:15:12 +0200

    E/S implementada

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * @brief Funcion que se encarga de la E/S 
  */
 int ent_sal(void){
-
+	int a;
+	
+	scanf("%d",&a);
+	printf("Num: %d\n",a);
 }
 
 int main(int argc,char** argv)
